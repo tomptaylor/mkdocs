7559f230fe37cc6d440a9ec3263b1862d1a9fb93
Tom Taylor <tomptaylor@gmail.com>
2025-01-01 10:08:54 -0700

    dont print the action as input

--- source/detail.sh
+++ source/detail.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 total_lines=$(wc -l < testdb.csv) 
-awk -F','  -v total_lines="$total_lines" 'BEGIN { print total_lines }{
-    if (NR<total_lines) {
-        table = $1; field = $2; filename = "./" table "/" table "-detail.md";  
+awk -F','  '{
+        table = $1; field = $2; filename = "./" table "/" table "-detail.md"; 
+        if (field != "Action") {
         print field "<input placeholder=\"field\" style=\"border: 1px solid #ccc; padding: 5px;\"><br><br>" >> filename  
     }}
     ' testdb.csv
